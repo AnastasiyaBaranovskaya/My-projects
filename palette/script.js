84c8ad07313fa44eb8aedf6eed27deec6ac0923d
Anastasiya <anastasiya.baranouskaya@gmail.com>
2019-04-30 12:30:42 +0300

    feat: implement color picker refactor: change structure of tools-pallete
    Signed-off-by: Anastasiya <anastasiya.baranouskaya@gmail.com>

--- palette/script.js
+++ palette/script.js
@@ -1,100 +1,95 @@
 const canvas = document.querySelectorAll(".canvas-items");
-var red = document.querySelector(".red-color");
-var blue = document.querySelector(".blue-color");
+const color = document.querySelectorAll(".color-static");
 const pan = document.querySelector(".paint-tool");
+const prevbutton = document.querySelector(".prev-color");
 var curColor = document.querySelector(".current-circle");
 var prevColor = document.querySelector(".prev-circle");
 const trans = document.querySelector(".transform");
 const move = document.querySelector(".move");
 const chooseCol = document.querySelector(".choose-color");
-
+//Paint bucket//
 pan.addEventListener('click', changeColor);
 
-function changeColor(e){
-red.addEventListener('click', color_red);
+function changeColor(e) {
+    for (let i = 0; i < canvas.length; i++) {
+        canvas[i].addEventListener('click', paint);
+    }
 
-function color_red(e){
-    prevColor.style.backgroundColor  = curColor.style.backgroundColor ;
-    curColor.style.backgroundColor = 'red';
-for (let i=0; i < canvas.length; i++){
-    canvas[i].addEventListener('click', paint);
+    function paint(e) {
+        e.target.style.backgroundColor = curColor.style.backgroundColor;
+        for (let i = 0; i < canvas.length; i++) {
+            canvas[i].removeEventListener('click', paint);
+        }
+    }
 }
-function paint(e){
-    e.target.style.backgroundColor = 'red';
-    for (let i=0; i < canvas.length; i++){
-      canvas[i].removeEventListener('click', paint);
-  }
-}
-}
+prevbutton.addEventListener('click', ChangeCurColor);
 
-blue.addEventListener('click', color_blue);
+function ChangeCurColor(e) {
+    curColor.style.backgroundColor = prevbutton.firstElementChild.style.backgroundColor;
+}
+for (let i = 0; i < color.length; i++) {
+    color[i].addEventListener('click', addColor);
 
-function color_blue(e){
-    prevColor.style.backgroundColor  = curColor.style.backgroundColor ;
-    curColor.style.backgroundColor = 'blue';
-for (let i=0; i < canvas.length; i++){
-    canvas[i].addEventListener('click', paint);
+    function addColor(e) {
+        prevColor.style.backgroundColor = curColor.style.backgroundColor;
+        curColor.style.backgroundColor = color[i].firstElementChild.style.backgroundColor;
+    }
 }
-
-function paint(e){
-    e.target.style.backgroundColor = 'blue';
-    for (let i=0; i < canvas.length; i++){
-      canvas[i].removeEventListener('click', paint);
-  }
-}
-}
-}
+// Move Tool//
 move.addEventListener('click', moveCanvas);
-function moveCanvas(e){
-for (let i=0; i < canvas.length; i++){
-canvas[i].onmousedown = function(e) { 
-    canvas[i].style.position = 'absolute';
-    moveAt(e);
-    document.body.appendChild(canvas[i]);
-  
-    canvas[i].style.zIndex = 1000;
 
-    function moveAt(e) {
-      canvas[i].style.left = e.pageX - canvas[i].offsetWidth / 2 + 'px';
-      canvas[i].style.top = e.pageY - canvas[i].offsetHeight / 2 + 'px';
-    }
+function moveCanvas(e) {
+    for (let i = 0; i < canvas.length; i++) {
+        canvas[i].onmousedown = function (e) {
+            canvas[i].style.position = 'absolute';
+            moveAt(e);
+            document.body.appendChild(canvas[i]);
+            canvas[i].style.zIndex = 1000;
 
-    document.onmousemove = function(e) {
-      moveAt(e);
+            function moveAt(e) {
+                canvas[i].style.left = e.pageX - canvas[i].offsetWidth / 2 + 'px';
+                canvas[i].style.top = e.pageY - canvas[i].offsetHeight / 2 + 'px';
+            }
+            document.onmousemove = function (e) {
+                moveAt(e);
+            }
+            canvas[i].onmouseup = function () {
+                document.onmousemove = null;
+                canvas[i].onmouseup = null;
+            }
+            for (let i = 0; i < canvas.length; i++) {
+                canvas[i].onmousedown = null;
+            }
+        }
     }
-    canvas[i].onmouseup = function() {
-      document.onmousemove = null;
-      canvas[i].onmouseup = null;
-    }
-    for (let i=0; i < canvas.length; i++){
-    canvas[i].onmousedown = null;
-
-    }
-  }
 }
-}
-
+//Transformation//
 trans.addEventListener('click', Changeform);
-function Changeform(e){
-for (let i=0; i < canvas.length; i++){
-    canvas[i].addEventListener('click', transorm);
-}
-function transorm(e){
-    e.target.style.borderRadius = '50%';
-    for (let i=0; i < canvas.length; i++){
-      canvas[i].removeEventListener('click', transorm);
-  }
-}
-}
-chooseCol.addEventListener('click', ChooseColor);
-function ChooseColor(e){
-  for (let i=0; i < canvas.length; i++){
-    canvas[i].addEventListener('click', savecolor);
-  }
-  function savecolor(e){
-    curColor.style.backgroundColor = e.target.style.backgroundColor;
-    for (let i=0; i < canvas.length; i++){
-      canvas[i].removeEventListener('click', savecolor);
+
+function Changeform(e) {
+    for (let i = 0; i < canvas.length; i++) {
+        canvas[i].addEventListener('click', transorm);
+    }
+
+    function transorm(e) {
+        e.target.style.borderRadius = '50%';
+        for (let i = 0; i < canvas.length; i++) {
+            canvas[i].removeEventListener('click', transorm);
+        }
     }
 }
+//Choose color//
+chooseCol.addEventListener('click', ChooseColor);
+
+function ChooseColor(e) {
+    for (let i = 0; i < canvas.length; i++) {
+        canvas[i].addEventListener('click', savecolor);
+    }
+
+    function savecolor(e) {
+        curColor.style.backgroundColor = e.target.style.backgroundColor;
+        for (let i = 0; i < canvas.length; i++) {
+            canvas[i].removeEventListener('click', savecolor);
+        }
+    }
 }
