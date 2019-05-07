48dfe1becec9cb5add77e173f3805653a72ba855
Anastasiya <anastasiya.baranouskaya@gmail.com>
2019-05-07 23:25:26 +0300

    fix: add corectly API
    Signed-off-by: Anastasiya <anastasiya.baranouskaya@gmail.com>

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 const ytForm = document.getElementById('yt-form');
 const keywordInput = document.getElementById('keyword-input');
 const videoList = document.getElementById('videoListContainer');
-var pageToken = '';
+const buttonsContainer = document.getElementById('buttonsContainer');
+
+const ITEMS_REQUESTED = 26;
+const CARDS_PER_SLIDE = 4;
  
 ytForm.addEventListener('submit', e => {
     e.preventDefault();
-    execute();
+    execute(keywordInput.value);
 });
-function loadClient() {
-    gapi.client.setApiKey("AIzaSyDn2Ybd0qz5tdqoC8AU8_ucMLj3mC6XOzc");
-    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-    .then(function() { console.log("GAPI client loaded for API"); },
-    function(err) { console.error("Error loading GAPI client for API", err); });
-}
-// Make sure the client is loaded before calling this method//
-function execute() {
+
+function execute(searchQuery) {
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=Ks-_Mh1QhMc&key=AIzaSyDn2Ybd0qz5tdqoC8AU8_ucMLj3mC6XOzc', true);
+    const baseUrl = "https://www.googleapis.com/youtube/v3/search";
+    const queryList = "maxResults=" + ITEMS_REQUESTED + "&part=snippet&q=" + searchQuery + "&key=AIzaSyDn2Ybd0qz5tdqoC8AU8_ucMLj3mC6XOzc";
+    const url = baseUrl + "?" + queryList;
+
+    xhr.open('GET', url, true);
    
     if (!xhr) {
         alert('Giving up :( Cannot create an XMLHTTP instance');
@@ -24,20 +25,23 @@ function execute() {
     }
     xhr.onreadystatechange = function(){
         if (this.readyState == 4 && this.status == 200) {
-            const searchString = keywordInput.value;
-            var list = gapi.client.youtube.search.list({
-                "part": "snippet",
-                "type": "video",
-                "maxResults": 15,
-                "q": searchString
-            })
+            const result = this.response && JSON.parse(this.response);
+            const list = result.items;
+           // var newElement = document.createElement('ul');
+            //let toRender = "";
             for(var i in list) {
-                if(list[i].title != undefined && list[i].id != undefined) {
-                    var newElement = document.createElement('li');
-                    newElement.innerHTML = '<li>'+ list[i].title +'('+ list[i].id +')</li>';
-                    videoList.appendChild(newElement);
+                if(list[i].snippet && list[i].id && list[i].id.videoId) {
+                    console.log(list[i]);
+                    //toRender += '<li>'+ list[i].snippet.title +'('+ list[i].id.videoId +')</li>';
+                } else {
+                    console.warn(list[i])
                 }
             }
+            //newElement.innerHTML = toRender;
+            //videoList.appendChild(newElement);
+            
+            renderButtons(CARDS_PER_SLIDE, list);
+            renderSlide(0, CARDS_PER_SLIDE, list);
         }
     };
     xhr.onerror = function() {
@@ -45,26 +49,55 @@ function execute() {
     }
     xhr.send();
 }
-gapi.load("client");
-
-/*.then(function(response) {
-    // Handle the results here (response.result has the parsed body).
-    const listItems = response.result.items;
-    if (listItems) {
-        listItems.forEach(item => {
-            const videoId = item.id.videoId;
-            const videoTitle = item.snippet.title;
-            var output += '<li>${videoTitle} (${videoId})</li>';
-        });
-        output += '</ul>';
-
-        if (response.result.prevPageToken) {
-            output += '<br><a class="paginate" href="#" data-id="${response.result.prevPageToken}" onclick="paginate(event, this)">Prev</a>';
-        }
-        if (response.result.nextPageToken) {
-            output += '<a href="#" class="paginate" data-id="${response.result.nextPageToken}" onclick="paginate(event, this">Next</a>';
-        }
-        // Output list
-        videoList.innerHTML = output;
+function prepareCardData(snippet, video) {
+    return {
+        id: video.videoId,
+        title: snippet.title,
+        description: snippet.description,
+        publicationDate: snippet.publishedAt,
+        imageUrl: snippet.thumbnails.default.url
     }
-} */
+}
+function renderCard(data) {
+    const wrapper = document.createElement('div');
+    const img = document.createElement('img');
+    img.src = data.imageUrl;
+    const title = document.createElement('h3');
+    title.innerText = data.title;
+    const description = document.createElement('a');
+    description.innerText = data.description;
+    description.href = "https://www.youtube.com/watch?v=" + data.id;
+
+    wrapper.appendChild(img);
+    wrapper.appendChild(title)
+    wrapper.appendChild(description)
+
+    return wrapper;
+}
+function renderSlide(position, itemsPerSlide, list) {
+    videoList.innerHTML = "";
+
+    const offset = position * itemsPerSlide;   
+    const newList = list.slice(offset, offset + itemsPerSlide); 
+
+    newList.forEach(function(item) {
+        const cardData = prepareCardData(item.snippet, item.id);
+        const card = renderCard(cardData);
+        videoList.appendChild(card);
+    })
+}
+
+function renderButtons(itemsPerSlide, list) {
+    const buttonsCount = Math.ceil(list.length / itemsPerSlide);
+    buttonsContainer.innerHTML = "";
+    for(let index = 0; index < buttonsCount; index++) {
+        const button = document.createElement('button');
+        button.dataset.slide = index;
+        button.innerText = "Slide " + index;
+        button.addEventListener('click', function(e) {
+            renderSlide(index, itemsPerSlide, list);
+        })
+        buttonsContainer.appendChild(button);
+    }
+}
+
