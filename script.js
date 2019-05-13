427d73b8e831547287a1d84dd99d2d246d92bfa1
Anastasiya <anastasiya.baranouskaya@gmail.com>
2019-05-13 03:38:28 +0300

    feat: add touchswipe
    Signed-off-by: Anastasiya <anastasiya.baranouskaya@gmail.com>

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const wrapInput = document.createElement('p');
 const keywordInput = document.createElement('input');
 const buttonsContainer = document.createElement('div');
 const videoList = document.createElement('div');
+const videoWrapper = document.createElement('div');
 const ITEMS_REQUESTED = 16;
 var CARDS_PER_SLIDE = 4;
-const width1200 = window.matchMedia("(max-width: 1200px)");
-const width500 = window.matchMedia("(max-width: 500px)");
+
 function init(){
     document.body.insertBefore(ytForm, document.body.firstChild);
     keywordInput.setAttribute('type', 'text');
@@ -14,26 +14,12 @@ function init(){
     keywordInput.className = "keyword-input";
     wrapInput.appendChild(keywordInput);
     ytForm.appendChild(wrapInput);
-    document.body.appendChild(videoList);
+    document.body.appendChild(videoWrapper);
+    videoWrapper.appendChild(videoList);
+    videoWrapper.className = "video-wrapper";
     videoList.className = "video-container";
     document.body.appendChild(buttonsContainer);
     buttonsContainer.className = "slaider";
-
-    function changeWidth(width1200) {
-        if (width1200.matches) { 
-            CARDS_PER_SLIDE = 2;
-        }
-    }
-    changeWidth(width1200); 
-    width1200.addListener(changeWidth);
-
-    function changeWidthmobile(width500) {
-        if (width500.matches) { 
-            CARDS_PER_SLIDE = 1;
-        } 
-    }
-    changeWidthmobile(width500); 
-    width500.addListener(changeWidthmobile);
 
     ytForm.addEventListener('submit', e => {
         e.preventDefault();
@@ -55,12 +41,13 @@ function execute(searchQuery) {
         return false;
     }
     xhr.onload = function(){
-        if (this.status == 200) {
+        const good = 200;
+        if (this.status == good) {
             resolve(this.response)
             const result = this.response && JSON.parse(this.response);
             const list = result.items;
             renderButtons(CARDS_PER_SLIDE, list);
-            renderSlide(0, CARDS_PER_SLIDE, list);
+            renderSlide(list);
         } else{
             var error = new Error(this.statusText);
             error.code = this.status;
@@ -110,32 +97,32 @@ function renderCard(data) {
 
     return wrapper;
 }
-function renderSlide(position, CARDS_PER_SLIDE, list) {
+function renderSlide(list) {
     while (videoList.firstChild){
     videoList.removeChild(videoList.firstChild);
     }
-    const offset = position * CARDS_PER_SLIDE;   
-    const newList = list.slice(offset, offset + CARDS_PER_SLIDE); 
-
-    newList.forEach(function(item) {
+    list.forEach(item => {
         const cardData = prepareCardData(item.snippet, item.id);
         const card = renderCard(cardData);
         videoList.appendChild(card);
-    })
+    });  
 }
-renderSlide();
 function renderButtons(CARDS_PER_SLIDE, list) {
     const buttonsCount = Math.ceil(list.length / CARDS_PER_SLIDE);
     while (buttonsContainer.firstChild){
         buttonsContainer.removeChild(buttonsContainer.firstChild);
     }
-    for(let index = 1; index < buttonsCount; index++) {
-        const button = document.createElement('button');
+    for(let index = 1; index <= buttonsCount; index++) {
+        const button = document.createElement('input');
+        button.setAttribute("type", "radio")
+        button.setAttribute("name", "slider-item")
         button.dataset.slide = index;
-        button.innerText = index;
-        button.addEventListener('click', function() {
-            renderSlide(index-1, CARDS_PER_SLIDE, list);
-        })
+       // button.addEventListener('click', function() {
+       //     renderSlide(index-1, CARDS_PER_SLIDE, list);
+       // })
         buttonsContainer.appendChild(button);
+        const inputcheck = document.body.querySelector("input[data-slide = '1']");
+        inputcheck.setAttribute("checked", "checked");
     }
 }
+
